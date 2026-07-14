c406a262080ee1a2f52fc64b8e1ec113eb4d89b9
mark2540 <markaroko203@gmail.com>
2026-07-14 08:51:49 +0300

    Create float.py

---
+++ float.py
@@ -0,0 +1,13 @@
+# float.py
+# Demonstrates float variables and decimal calculations
+
+x = 7.5
+y = 2.3
+
+print("Sum:", x + y)
+print("Difference:", x - y)
+print("Product:", x * y)
+print("Quotient:", x / y)
+print("Rounded quotient:", round(x / y, 2))
+print("Modulus:", x % y)
+print("Floor Division:", x // y)
